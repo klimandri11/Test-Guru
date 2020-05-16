9d647f40d6ea3ce6826d8f69be3f7027525c6177
klimandri11 <andrey.klimenko.99@mail.ru>
2020-05-16 16:46:52 +0300

    Создал метод класса Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,5 @@
 class Test < ApplicationRecord
-  
+  def self.tests_titels(category)
+    Test.joins(:categories).where(:categories { title: category }).order(title: :desc).select(:title)
+  end
 end
