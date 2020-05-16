3d44fae259c3361b4d613c8f6434498313e3406d
klimandri11 <andrey.klimenko.99@mail.ru>
2020-05-16 16:19:03 +0300

    Создал набор начальных данных и инстанс-метод в модели User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  def passed_tests(level)
+    tests.where(level: level)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{ name: 'Andrey' }, { name: 'Lera' }])
+
+categories = Category.create([{ title: 'SQL' }, { title: 'NOSQL' }, { title: 'Rails' }])
+
+tests = Test.create([{ title: 'SQL', level: 0, category_id: 1, user_id: 1 },
+  { title: 'ArangoDB', level: 1, category_id: 2, user_id: 2 },
+  { title: 'Rails', level: 2, category_id: 3, user_id: 1 }])
+
+questions = Question.create([
+  { text: 'Каким типом баз данных позволяет управлять язык SQL?', test_id: 1 },
+  { text: 'Что представляет собой база данных? ', test_id: 2 },
+  { text: 'Под какую платформу написан Rails?', test_id: 3 },
+  { text: 'Какой язык запросов используется? ', test_id: 2 }])
+
+answers = Answer.create([{ text: 'Реляционными', correct: true, question_id: 1 },
+  { text: 'Множество коллекций', correct: true, question_id: 2 },
+  { text: 'AQL', correct: true, question_id: 2 },
+  { text: 'ios', question_id: 3 }])
