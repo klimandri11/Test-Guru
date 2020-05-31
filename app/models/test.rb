b121c0c2e7b6620e02335333fa45f9d872651c34
klimandri11 <andrey.klimenko.99@mail.ru>
2020-05-31 17:01:19 +0300

    add scope metods

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,10 +6,15 @@ class Test < ApplicationRecord
   has_many :completed_tests, dependent: :destroy
   has_many :users, through: :completed_tests
 
-  def self.tests_titels(category)
-    Test.joins(:categories)
-      .where(categories: { title: category })
-      .order(title: :desc)
-      .pluck(:title)
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :tests_titels, -> (category) { joins(:category)
+                                       .where(categories: { title: category })
+                                       .order(title: :desc)
+                                      }
+
+  def self.tests_titels_array_name(category)
+    tests_titels.pluck(:title)
   end
 end
