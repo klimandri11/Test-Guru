8702aaf4544c793c8c2824888ef6fa425dafd0dc
klimandri11 <andrey.klimenko.99@mail.ru>
2020-06-07 08:33:11 +0300

    добавление контроллера тестов

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,34 +1,39 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_test, only: %i[index create]
-  before_action :find_question, only: %i[show destroy]
+  before_action :find_test, only: %i[create new]
+  before_action :find_question, only: %i[show destroy edit update]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def index
-    result = "Вопросы теста №#{@test.id}:\n"
-    @test.questions.each do |question|
-      result += "#{question.text}\n"
-    end
+  def show; end
 
-    render plain: result
+  def new
+    @question = @test.questions.new
   end
 
-  def show
-    result = "Вопрос #{@question.id}:\n#{@question.text}\n "
-    render plain: result
-  end
-
-  def new; end
+  def edit; end
 
   def create
-    @question = @test.questions.create!(question_params)
-    render plain: 'Создан новый вопрос'
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
   end
 
   def destroy
     @question.destroy!
-    render plain: 'Вопрос удален'
+    redirect_to tests_path(@question.test)
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
   end
 
   private
