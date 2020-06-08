63a2eecb02a43d628839a40024ed2b9e35c414d0
klimandri11 <andrey.klimenko.99@mail.ru>
2020-06-08 09:21:02 +0300

    version 0.7.0

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+
+  def question_header(question)
+    if question.new_record?
+      "Creating a new question in the test #{question.test.title}"
+    else
+      "Editing a question in a test #{question.test.title}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'tests/new'
-  get 'tests/edit'
-  get 'tests/show'
-  get 'tests/update'
-  get 'tests/index'
-  get 'tests/create'
-  get 'tests/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tests do
     resources :questions, shallow: true
