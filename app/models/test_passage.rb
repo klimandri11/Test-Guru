838760293ffd5cd1007b8931e14ef9c4865835b2
klimandri11 <andrey.klimenko.99@mail.ru>
2020-06-21 14:28:38 +0300

    version 0.8.1

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,8 +3,7 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_validation :before_validation_set_next_question, on: :update
+  before_validation :before_validation_set_current_question
 
   def completed?
     current_question.nil?
@@ -27,17 +26,25 @@ class TestPassage < ApplicationRecord
   end
 
   def number_current_question
-    test.questions.count - test.questions.order(:id).where('id > ?', current_question.id).count
+    test.questions.count - remaining_questions.count
   end
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def before_validation_set_current_question
+    self.current_question = next_question
   end
 
-  def before_validation_set_next_question
-    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+  def next_question
+    if current_question
+      remaining_questions.first
+    else
+      test.questions.first
+    end
+  end
+
+  def remaining_questions
+    test.questions.order(:id).where('id > ?', current_question.id)
   end
 
   def correct_answer?(answer_ids)
