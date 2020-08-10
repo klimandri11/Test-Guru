0a3dcdaffb9a18ee627c489e6b00f2b3c89e3a40
klimandri11 <andrey.klimenko.99@mail.ru>
2020-08-10 13:21:39 +0300

    version 0.10.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,14 @@
 class User < ApplicationRecord
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\z/i
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages
+  has_many :created_tests, inverse_of: :author, class_name: 'Test', foreign_key: 'author_id'
 
   devise :database_authenticatable,
          :registerable,
          :recoverable,
          :rememberable,
          :validatable
-
-  has_many :test_passages, dependent: :destroy
-  has_many :tests, through: :test_passages
-  has_many :created_tests, inverse_of: :author, class_name: 'Test', foreign_key: 'author_id'
-
-  validates :email, presence: true,
-                    uniqueness: true,
-                    format: { with: VALID_EMAIL_REGEX }
 
   def passed_tests(level)
     tests.where(level: level)
